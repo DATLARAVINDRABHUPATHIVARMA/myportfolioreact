03d84a3dd99061a0d3b17bd68f5b3c299389aab6
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2024-12-30 08:36:20 +0530

    weather project added

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,4 +1,5 @@
 import styles from "./ProjectsStyles.module.css";
+import weather from '../../assets/weather.png'
 import viberr from "../../assets/viberr.png";
 import freshBurger from "../../assets/fresh-burger.png";
 import hipsster from "../../assets/hipsster.png";
@@ -11,26 +12,32 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
+          src={weather}
+          link="https://weathercalculatorapprox.vercel.app/"
+          h3="Approx Weather"
+          p="Weather App"
+        />
+        <ProjectCard
           src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://github.com/Ade-mir/html-css-js-portfolio-tutorial-2"
           h3="Viberr"
           p="Streaming App"
         />
         <ProjectCard
           src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://github.com/Ade-mir/react-js-personal-portfolio"
           h3="Fresh Burger"
           p="Hamburger Restaurant"
         />
         <ProjectCard
           src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://github.com/Ade-mir/snake-game-js"
           h3="Hipsster"
           p="Glasses Shop"
         />
         <ProjectCard
           src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://github.com/Ade-mir/react-portfolio-v2"
           h3="FitLift"
           p="Fitness App"
         />
