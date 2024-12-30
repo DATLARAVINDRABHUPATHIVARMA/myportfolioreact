da669fabb289b21b499afd25361f8e6aa4a3057e
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2024-12-30 20:54:09 +0530

    skills complete

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,23 +1,22 @@
-import styles from './SkillsStyles.module.css';
-import html from '../../assets/Skill Icons/html.png';
-import css from '../../assets/Skill Icons/css.png';
-import javascript from '../../assets/Skill Icons/javascript.png';
-import python from '../../assets/Skill Icons/python.png';
-import node from '../../assets/Skill Icons/node.png';
-import react from '../../assets/Skill Icons/react.png';
-import next from '../../assets/Skill Icons/nextjs.png';
-import redux from '../../assets/Skill Icons/redux.png';
-import vue from '../../assets/Skill Icons/vue.png';
-import tailwind from '../../assets/Skill Icons/tailwind.png';
-import git from '../../assets/Skill Icons/git.png';
-import flask from '../../assets/Skill Icons/flask.png';
-import mysql from '../../assets/Skill Icons/mysql.png';
-import django from '../../assets/Skill Icons/django.png';
-import bootstrap from '../../assets/Skill Icons/bootstrap.png';
-import SkillList from '../../common/SkillList';
+import styles from "./SkillsStyles.module.css";
+import html from "../../assets/Skill Icons/html.png";
+import css from "../../assets/Skill Icons/css.png";
+import javascript from "../../assets/Skill Icons/javascript.png";
+import python from "../../assets/Skill Icons/python.png";
+import node from "../../assets/Skill Icons/node.png";
+import react from "../../assets/Skill Icons/react.png";
+import next from "../../assets/Skill Icons/nextjs.png";
+import redux from "../../assets/Skill Icons/redux.png";
+import vue from "../../assets/Skill Icons/vue.png";
+import tailwind from "../../assets/Skill Icons/tailwind.png";
+import git from "../../assets/Skill Icons/git.png";
+import flask from "../../assets/Skill Icons/flask.png";
+import mysql from "../../assets/Skill Icons/mysql.png";
+import django from "../../assets/Skill Icons/django.png";
+import bootstrap from "../../assets/Skill Icons/bootstrap.png";
+import SkillList from "../../common/SkillList";
 
 function Skills() {
-
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
