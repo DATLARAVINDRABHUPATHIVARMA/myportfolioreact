1bc1a39d38ffb79fb12e3c7fcb5588c4ac3b1dee
DATLA RAVINDRA BHUPATHI VARMA <ravivarmadatla64@gmail.com>
2024-12-31 15:52:31 +0530

    footer

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import styles from './FooterStyles.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        &copy; 2025 DATLA RAVINDRA BHUPATHI VARMA. <br />
+        All rights reserved.
+      </p>
+    </section>
+  );
+}
+
+export default Footer;
